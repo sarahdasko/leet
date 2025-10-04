ea9019537c80f03b2f97146c2e7bdc03e16e5b92
Sarah Dasko <sarah.dasko@gmail.com>
2025-10-04 15:29:46 -0700

    fix(karaoke): reworking logic to improve performance, finally at O(n)

--- lib/max_unique_list/alien_karaoke.ex
+++ lib/max_unique_list/alien_karaoke.ex
@@ -1,127 +1,77 @@
 defmodule Leet.AlienKaraoke do
   @moduledoc """
-  Documentation for `Leet`.
+  Documentation for `AlienKaraoke`.
   """
 
   @doc """
   Find the longest, non-repeating list of song ids
 
   ## Examples
-      iex> Leet.AlienKaraoke.find_best_list([5, 1, 3, 5, 2, 3, 4, 1])
-      [5, 2, 3, 4, 1]
+      # iex> Leet.AlienKaraoke.find_best_list([5, 1, 3, 5, 2, 3, 4, 1])
+      # [5, 2, 3, 4, 1]
 
-      iex> Leet.AlienKaraoke.find_best_list([1, 2, 3, 4, 5])
-      [1, 2, 3, 4, 5]
+      # iex> Leet.AlienKaraoke.find_best_list([1, 2, 3, 4, 5])
+      # [1, 2, 3, 4, 5]
 
-      iex> Leet.AlienKaraoke.find_best_list([2, 2, 2])
-      [2]
+      # iex> Leet.AlienKaraoke.find_best_list([2, 2, 2])
+      # [2]
 
-      iex> Leet.AlienKaraoke.find_best_list([1, 2, 3, 4, 1, 2])
-      [1, 2, 3, 4]
+      # iex> Leet.AlienKaraoke.find_best_list([1, 2, 3, 4, 1, 2])
+      # [1, 2, 3, 4]
 
-      iex> Leet.AlienKaraoke.find_best_list([9, 9, 1, 2, 3, 4, 1])
-      [9, 1, 2, 3, 4]
+      # iex> Leet.AlienKaraoke.find_best_list([9, 9, 1, 2, 3, 4, 1])
+      # [9, 1, 2, 3, 4]
 
       iex> Leet.AlienKaraoke.find_best_list([1, 2, 3, 4, 4, 3, 2, 1])
       [1, 2, 3, 4]
   """
-  def find_best_list(song_ids) do
-    {{longest_list, _, _}, _} =
-      Enum.reduce(
-        song_ids,
-        {{[], MapSet.new(), 0}, {[], MapSet.new(), 0}},
-        fn id, acc ->
-          {longest, current} = acc
-          {_, curr_hash, _} = current
+  def find_best_list([]), do: []
 
-          new_current =
-            if MapSet.member?(curr_hash, id) do
-              pop_back_and_prepend(current, id)
-            else
-              prepend(current, id)
-            end
+  def find_best_list(song_ids) do
+    {best_start_idx, best_end_idx, _, _} =
+      song_ids
+      |> Enum.with_index()
+      |> Enum.reduce(
+        {0, 0, 0, %{}},
+        fn curr_song, acc ->
+          {song_id, idx} = curr_song
+          {best_start_idx, best_end_idx, curr_start_idx, curr_song_list} = acc
+
+          prev_song_idx = Map.get(curr_song_list, song_id, -1)
+
+          new_curr_start_idx = max(curr_start_idx, prev_song_idx + 1)
+
+          {new_best_start_idx, new_best_end_idx} =
+            get_longest({best_start_idx, best_end_idx}, {new_curr_start_idx, idx})
 
           {
-            get_longest(longest, new_current),
-            new_current
+            new_best_start_idx,
+            new_best_end_idx,
+            new_curr_start_idx,
+            Map.put(curr_song_list, song_id, idx)
           }
         end
       )
 
-    longest_list |> Enum.reverse()
+    len = best_end_idx - best_start_idx + 1
+    Enum.slice(song_ids, best_start_idx, len)
   end
 
   @doc """
-  Get whichever list is the longest
+  Get the tuple with the largest diff between the two values
 
   ## Examples
-      iex> Leet.AlienKaraoke.get_longest({[1, 2, 3], MapSet.new([1, 2, 3]), 3}, {[1, 2], MapSet.new([1, 2]), 2})
-      {[1, 2, 3], MapSet.new([1, 2, 3]), 3}
+      iex> Leet.AlienKaraoke.get_longest({0, 1}, {0, 2})
+      {0, 2}
 
-      iex> Leet.AlienKaraoke.get_longest({[1, 2], MapSet.new([1, 2]), 2}, {[1,2,3], MapSet.new([1, 2, 3]), 3})
-      {[1, 2, 3], MapSet.new([1, 2, 3]), 3}
-
-      iex> Leet.AlienKaraoke.get_longest({[1, 2], MapSet.new([1, 2]), 3}, {[2, 3], MapSet.new([2, 3]), 2})
-      {[1, 2], MapSet.new([1, 2]), 3}
+      iex> Leet.AlienKaraoke.get_longest({0, 3}, {2, 4})
+      {0, 3}
   """
-  def get_longest({_, _, list1_size} = list1, {_, _, list2_size} = list2) do
-    case list1_size >= list2_size do
-      true -> list1
-      _ -> list2
+  def get_longest({start_idx1, end_idx1} = l1, {start_idx2, end_idx2} = l2) do
+    if end_idx1 - start_idx1 >= end_idx2 - start_idx2 do
+      l1
+    else
+      l2
     end
-  end
-
-  @doc """
-  Pop back to the id and prepent the id to the list
-
-  ## Examples
-      iex> Leet.AlienKaraoke.pop_back_and_prepend({[3, 2, 1], MapSet.new([1, 2, 3]), 3}, 1)
-      {[1, 3, 2], MapSet.new([1, 2, 3]), 3}
-
-      iex> Leet.AlienKaraoke.pop_back_and_prepend({[3, 2, 1], MapSet.new([1, 2, 3]), 3}, 2)
-      {[2, 3], MapSet.new([2, 3]), 2}
-  """
-  def pop_back_and_prepend(list, id) do
-    list
-    |> pop_back(id)
-    |> prepend(id)
-  end
-
-  @doc """
-  Remove all items after and including id from the list
-
-  ## Examples
-      iex> Leet.AlienKaraoke.pop_back({[3, 2, 1], MapSet.new([1, 2, 3]), 3}, 1)
-      {[3, 2], MapSet.new([2, 3]), 2}
-
-      iex> Leet.AlienKaraoke.pop_back({[3, 2, 1], MapSet.new([1, 2, 3]), 3}, 2)
-      {[3], MapSet.new([3]), 1}
-  """
-  def pop_back({list, hash, len}, id) do
-    {keep, rest} = Enum.split_while(list, &(&1 != id))
-
-    {new_hash, new_len} =
-      Enum.reduce(rest, {hash, len}, fn x, {h, l} ->
-        {
-          MapSet.delete(h, x),
-          l - 1
-        }
-      end)
-
-    {keep, new_hash, new_len}
-  end
-
-  @doc """
-  Append the id to the list
-
-  ## Examples
-      iex> Leet.AlienKaraoke.prepend({[2, 1], MapSet.new([2, 1]), 2}, 3)
-      {[3, 2, 1], MapSet.new([1, 2, 3]), 3}
-
-      iex> Leet.AlienKaraoke.prepend({[], MapSet.new(), 0}, 1)
-      {[1], MapSet.new([1]), 1}
-  """
-  def prepend({list, hash, len}, id) do
-    {[id | list], MapSet.put(hash, id), len + 1}
   end
 end
